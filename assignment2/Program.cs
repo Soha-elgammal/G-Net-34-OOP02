a297130ab67427c8d86cf4ee558f49b3995b976a
Soha-elgammal <sohafathy226@gmail.com>
2026-02-21 20:20:42 +0200

    part 1 - Q1 :

--- assignment2/Program.cs
+++ assignment2/Program.cs
@@ -1,10 +1,64 @@
 ﻿namespace assignment2
 {
+    
+    #region Q1
+    // Q1 - a
+    // Problems in the original design:
+    // 1- Owner and Balance are public (break encapsulation)
+    // 2- Withdraw has no validation (can allow negative balance)
+
+    // Q1 - b
+    // Fixed version using proper encapsulation
+    //public class BankAccount
+    //{
+    //    private string owner;
+    //    private double balance;
+
+    //    public string Owner
+    //    {
+    //        get { return owner; }
+    //        set
+    //        {
+    //            if (!string.IsNullOrEmpty(value))
+    //                owner = value;
+    //        }
+    //    }
+
+    //    public double Balance
+    //    {
+    //        get { return balance; }
+    //        private set
+    //        {
+    //            if (value >= 0)
+    //                balance = value;
+    //        }
+    //    }
+
+    //    public void Withdraw(double amount)
+    //    {
+    //        if (amount > 0 && amount <= balance)
+    //            balance -= amount;
+    //    }
+    //}
+    // Q1 - c
+    // Why public fields are bad:
+    // - Break encapsulation
+    // - Allow invalid values
+    // - Hard to control or modify behavior later
+
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            // part 1 
+            // Q1 :
+            //BankAccount account = new BankAccount();
+
+            //account.Owner = "Soha";
+            //account.Withdraw(50);
+
+            //Console.WriteLine("Owner: " + account.Owner);
         }
     }
 }
